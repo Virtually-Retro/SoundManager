1a096981a716dcec66ffa88287c52d99623f187c
Ryn <ryn.carmichael@gmail.com>
2024-11-06 08:08:00 +1000

    Godot 4.x Simple MP3 Audio Manager v1.1
    Version 1.1 of Ryn's Simple MP3 Audio Manager for Godot 4.x.

--- soundManager.gd
+++ soundManager.gd
@@ -1,5 +1,5 @@
 #----------------------------------------------------
-# MP3 Simple SoundManager by Ryn (c) 2024 Version 1.0
+# MP3 Simple SoundManager by Ryn (c) 2024 Version 1.1
 # MIT License
 #----------------------------------------------------
 extends Node
@@ -11,13 +11,14 @@ extends Node
 
 
 func add_sound(filename: String) -> int: # Returns the unique ID of the loaded sound -1 is an error
-	if filename.right(3).to_lower() != "mp3": # Check for MP3 extension
+	filename = filename.to_lower()
+	if not filename.ends_with("mp3"): # Check for MP3 extension
 		return -1 # You should check for -1 return as an error
-		
+	
 	var soundNodeName: String = filename.replacen(".","_")
 	if soundNodeNames.find(soundNodeName) > -1:  # Stops loading multiples of same file
 		return soundNodeNames.find(soundNodeName) # Returns the ID of existing loaded file
-		
+	
 	var soundNode: AudioStreamPlayer = AudioStreamPlayer.new()
 	soundNode.stream = load_mp3(filename);
 	if soundNode.stream != null: # check for no valid MP3 data
@@ -31,40 +32,78 @@ func add_sound(filename: String) -> int: # Returns the unique ID of the loaded s
 
 
 func clear_all_sounds() -> void: # For clearing all sound when needed
-	if is_inside_tree():
-		get_tree().call_group(groupName, "queue_free")
+	if is_inside_tree(): get_tree().call_group(groupName, "queue_free")
 	soundNodeNames.clear()
 
 
-func play_sound(soundID: int) -> void:
-	if soundID not in range(soundNodeNames.size()): return
+func get_sound_id_by_name(filename: String) -> int:
+	filename = filename.to_lower().replacen(".","_")
+	for i: int in range(soundNodeNames.size()):
+		if soundNodeNames[i] == filename:
+			return i
+	return -1
 
-	var soundNode: AudioStreamPlayer = get_node(soundNodeNames[soundID])
+
+func get_sound_status(soundID: int) -> int:
+	if soundID not in range(soundNodeNames.size()): return -1
+	var soundNode: AudioStreamPlayer = get_node_or_null(soundNodeNames[soundID])
 	if soundNode != null:
-		soundNode.play()
+		if soundNode.playing: return 1
+		if soundNode.stream_paused: return 2
+		if not soundNode.playing : return 0
+		return -1
+	else:
+		return -1
+
+
+func play_sound(soundID: int, pitch_shift: bool) -> void:
+	if soundID not in range(soundNodeNames.size()): return
+	var soundNode: AudioStreamPlayer = get_node_or_null(soundNodeNames[soundID])
+	if soundNode != null: 
+		if soundNode.playing: 
+			soundNode.stream_paused = true
+			return
+		if soundNode.stream_paused:
+			soundNode.stream_paused = false
+			return
+		if pitch_shift:
+			soundNode.pitch_scale = randf_range(0.8, 1.2)
+			soundNode.play()
+		else:
+			soundNode.pitch_scale = 1
+			soundNode.play()
+
+
+func set_volume(vol_level: int) -> void:
+	# Check / Correct / Calc volume for liner_to_db
+	if vol_level < 0: vol_level = 0
+	if vol_level > 100: vol_level = 100
+	var set_vol_level: float = 0.01 * vol_level
+	# Set volume or all audio nodes
+	for i: int in soundNodeNames.size():
+		var soundNode: AudioStreamPlayer = get_node_or_null(soundNodeNames[i])
+		if soundNode != null:
+			soundNode.volume_db = linear_to_db(set_vol_level)
 
 
 func stop_sound(soundID: int) -> void:
 	if soundID not in range(soundNodeNames.size()): return
-
-	var soundNode: AudioStreamPlayer = get_node(soundNodeNames[soundID])
-	if soundNode != null:
+	var soundNode: AudioStreamPlayer = get_node_or_null(soundNodeNames[soundID])
+	if soundNode != null: 
 		soundNode.stop()
 
 
 func pause_sound(soundID: int) -> void:
 	if soundID not in range(soundNodeNames.size()): return
-
-	var soundNode: AudioStreamPlayer = get_node(soundNodeNames[soundID])
-	if soundNode != null:
+	var soundNode: AudioStreamPlayer = get_node_or_null(soundNodeNames[soundID])
+	if soundNode != null: 
 		soundNode.stream_paused = true
 
 
 func resume_sound(soundID: int) -> void:
 	if soundID not in range(soundNodeNames.size()): return
-
-	var soundNode: AudioStreamPlayer = get_node(soundNodeNames[soundID])
-	if soundNode != null:
+	var soundNode: AudioStreamPlayer = get_node_or_null(soundNodeNames[soundID])
+	if soundNode != null: 
 		soundNode.stream_paused = false
 
 
