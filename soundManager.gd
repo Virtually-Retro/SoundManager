50f32e0357d0e54c1639d531d3fd9f8c289ad896
Ryn <ryn.carmichael@gmail.com>
2024-11-11 04:35:22 +1000

    Godot 4.x Simple MP3 Audio Manager v1.3
    Updated internal functions and added global volume control.

--- soundManager.gd
+++ soundManager.gd
@@ -1,16 +1,18 @@
 #----------------------------------------------------
-# MP3 Simple SoundManager by Ryn (c) 2024 Version 1.2
+# MP3 Simple SoundManager by Ryn (c) 2024 Version 1.3
 # MIT License
 #----------------------------------------------------
 extends Node
 
 
 @onready var soundNodeNames: Array[String] = []
+@onready var soundDefaultVolume: float = 0.5
 @onready var soundFilesPath: String = "res://sounds/" # Change path to suit 
 @onready var groupName: String = "sounds" # Change group to suit
 
 
-# load a passed audio stream name
+# Adds a sound stream to the database check for prior existance
+# and loading if required.
 func add_sound(filename: String) -> int: # Returns the unique ID of the loaded sound -1 is an error
 	filename = filename.to_lower()
 	if not filename.ends_with("mp3"): # Check for MP3 extension
@@ -21,10 +23,11 @@ func add_sound(filename: String) -> int: # Returns the unique ID of the loaded s
 		return soundNodeNames.find(soundNodeName) # Returns the ID of existing loaded file
 	
 	var soundNode: AudioStreamPlayer = AudioStreamPlayer.new()
-	soundNode.stream = load_mp3_audio_stream(filename);
+	soundNode.stream = _load_mp3_audio_stream(filename);
 	if soundNode.stream != null: # check for no valid MP3 data
 		soundNode.name = soundNodeName
-		soundNode.add_to_group(groupName, true)
+		soundNode.volume_db = linear_to_db(soundDefaultVolume)
+		soundNode.add_to_group(soundGroupName, true)
 		soundNodeNames.append(soundNodeName)
 		add_child(soundNode)
 		return soundNodeNames.size() - 1
@@ -34,14 +37,14 @@ func add_sound(filename: String) -> int: # Returns the unique ID of the loaded s
 
 # For clearing all streams and the database when needed
 func clear_all_sounds() -> void:
-	if is_inside_tree(): get_tree().call_group(groupName, "queue_free")
+	if is_inside_tree(): get_tree().call_group(soundGroupName, "queue_free")
 	soundNodeNames.clear()
 
 
 # For playing an audio file by name.  If the stream is not loaded
-# if will load and play the sound, you should use the ID from that
-# Point onward.  This function is useful so all streams don't need to
-# all be loaded at the start.
+# it will load and play the sound, you should use the ID from that
+# point onward.  This function is useful so all streams can be loaded
+# as needed.
 func play_sound_by_name(filename: String, pitch_shift: bool) -> void:
 	var soundID: int = get_sound_id_by_name(filename)
 	if soundID > -1: # Sound Stream found
@@ -55,11 +58,11 @@ func play_sound_by_name(filename: String, pitch_shift: bool) -> void:
 # For returning the unique ID of a loaded Sound
 # Improved using array search method
 func get_sound_id_by_name(filename: String) -> int:
-	filename = filename.to_lower().replacen(".","_")
-	return soundNodeNames.find(filename)
+	var soundNodeName: String = filename.to_lower().replacen(".","_")
+	return soundNodeNames.find(soundNodeName)
 
 
-# For getting the play status of a load stream
+# For getting the play status of a loaded stream
 func get_sound_status(soundID: int) -> int:
 	if soundID not in range(soundNodeNames.size()): return -1
 	var soundNode: AudioStreamPlayer = get_node_or_null(soundNodeNames[soundID])
@@ -96,14 +99,14 @@ func play_sound(soundID: int, pitch_shift: bool) -> void:
 # Also corrects and adjusts the volume level to linear DB
 func set_volume(vol_level: int) -> void:
 	# Check / Correct / Calc volume for liner_to_db
-	if vol_level < 0: vol_level = 0
-	if vol_level > 100: vol_level = 100
-	var set_vol_level: float = 0.01 * vol_level
+	if vol_level not in range(0,101):
+		vol_level = 50
+	soundDefaultVolume = 0.01 * vol_level
 	# Set volume or all audio nodes
 	for i: int in soundNodeNames.size():
 		var soundNode: AudioStreamPlayer = get_node_or_null(soundNodeNames[i])
 		if soundNode != null:
-			soundNode.volume_db = linear_to_db(set_vol_level)
+			soundNode.volume_db = linear_to_db(soundDefaultVolume)
 
 
 # Stops a loaded audio stream from playing
@@ -131,7 +134,7 @@ func resume_sound(soundID: int) -> void:
 
 
 # Load the MP3 audio stream data, only used internaly.
-func load_mp3_audio_stream(filename: String) -> AudioStream:
+func _load_mp3_audio_stream(filename: String) -> AudioStream:
 	var file: Object = FileAccess.open(soundFilesPath + filename, FileAccess.READ)
 	if file != null: # Check for valid file
 		if file.get_length() == 0: # Check for data
